f20e306bcdfc15ae254daaa17c020e7e00be970b
Kairo0628 <112845598+Kairo0628@users.noreply.github.com>
2025-12-17 11:42:37 +0900

    Update comments and file save path in candle code

--- batch/dags/candle_1hour_dag.py
+++ batch/dags/candle_1hour_dag.py
@@ -4,15 +4,15 @@ from airflow.decorators import task
 from CandlePlugin import transform_and_load_to_s3
 from CandlePlugin import BASE_URL, MARKETS, DEFAULT_PARAMS
 
-from datetime import datetime, date
+from datetime import datetime
 import requests
 import time
 import logging
 
 with DAG(
     dag_id = '1hour_candle',
-    start_date = datetime(2025, 12, 15),
-    schedule = '2 0 * * *', # 한국 시간은 +9시,
+    start_date = datetime(2025, 12, 17),
+    schedule = '32 0 * * *', # 한국 시간은 +9시,
     catchup = False,
     tags = ["upbit", "candle", "raw"]
 ) as dag:
@@ -21,12 +21,12 @@ with DAG(
         
         수집 날짜(0시, 한국은 9시)를 가져옵니다.
         날짜를 기준으로 24시간 이전까지의 데이터를 가져와 코인별 24개의 데이터를 추출합니다.
-        추출된 데이터는 json 형태로 파싱하여 S3에 적재됩니다.
+        추출된 데이터는 parquet 형태로 파싱하여 S3에 적재됩니다.
     '''
 
     @task
     def extract():
-        today = date.today().strftime('%Y-%m-%dT00:00:00')
+        today = datetime.today().strftime('%Y-%m-%dT00:00:00')
         url = BASE_URL + 'minutes/5'
         market = MARKETS.copy()
         params = DEFAULT_PARAMS.copy()
@@ -39,7 +39,7 @@ with DAG(
                 params['market'] = m
                 response = requests.get(url = url, params = params)
                 data = response.json()
-                all_market_data.append({'market': m, 'raw': data})
+                all_market_data = all_market_data + data
             except Exception as e:
                 logging.info(f'Extract Error. Coin name: {m}')
                 print(f'Extract Error. Coin name: {m}')
@@ -50,4 +50,4 @@ with DAG(
         return all_market_data
     
     all_market_data = extract()
-    transform_and_load_to_s3(all_market_data, prefix = 'candle_1hour')
+    transform_and_load_to_s3(all_market_data, key = 'candle_1hour')
