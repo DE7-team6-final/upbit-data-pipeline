af9f984b61c1f71765b010327f0f5cc0564b9298
sally7788 <0702sally@gmail.com>
2025-12-18 17:39:34 +0900

    fileName edit

--- batch/dags/kis_stock_dag.py
+++ batch/dags/kis_stock_dag.py
@@ -123,7 +123,7 @@ def upload_to_s3(**context):
     
     now = datetime.now()
     file_name = "stock"
-    s3_key = f"{now.strftime('%Y-%m-%d')}/{file_name}"
+    s3_key = f"yymmdd={now.strftime('%Y-%m-%d')}/{file_name}"
     bucket_name = "team6-batch"  
 
     # 메모리에서 Parquet 변환 후 업로드
