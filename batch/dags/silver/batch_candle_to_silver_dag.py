48f70cc6494d82d13655479dd35bbac2e0f9c9a8
yeoreums <yeoreum.mail@gmail.com>
2025-12-19 11:03:51 +0900

    feat: make S3 -> Snowflake Silver DAG daily (#45)

--- batch/dags/silver/batch_candle_to_silver_dag.py
+++ batch/dags/silver/batch_candle_to_silver_dag.py
@@ -187,7 +187,7 @@ def load_candles_to_snowflake(ds, **context):
     # -------------------------------------------------------------------
     conn = snowflake_hook.get_conn()
     cur = conn.cursor()
-
+    cur.execute("USE WAREHOUSE DATA_TEAM_WH")
     cur.execute("USE DATABASE UPBIT_DB")
     cur.execute("USE SCHEMA SILVER")
 
@@ -197,6 +197,7 @@ def load_candles_to_snowflake(ds, **context):
         table_name=TARGET_TABLE,
         database="UPBIT_DB",
         schema="SILVER",
+        use_logical_type=True,
     )
 
     logging.info(
@@ -211,7 +212,7 @@ with DAG(
     dag_id="batch_candle_to_silver_dag",
     default_args=default_args,
     description="Load batch candle data from S3 into Snowflake Silver layer",
-    schedule_interval=None,
+    schedule_interval="@daily",
     catchup=False,
     max_active_runs=1,
     concurrency=1,
